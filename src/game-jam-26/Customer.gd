a1b6b76275b6e86cbd0f8a3a4bf7f3b124c7d165
Jo Johnson <jojohnson.jsj@gmail.com>
2026-03-02 16:50:14 -0800

    Customer scene

--- src/game-jam-26/Customer.gd
+++ src/game-jam-26/Customer.gd
@@ -1,11 +1,40 @@
 extends Area2D
 
+signal order_placed(item_type)
+signal patience_expired
+signal finished_eating
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+enum State { WAITING_FOR_PLAYER, ORDER_TAKEN, EATING }
 
+var current_state = State.WAITING_FOR_PLAYER
+var order_item = "pie"
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _ready():
+	$PatienceTimer.wait_time = 30.0
+	$EatingTimer.wait_time = 10.0
+	$PatienceTimer.timeout.connect(_on_patience_expired)
+	$EatingTimer.timeout.connect(_on_finished_eating)
+	$PatienceTimer.start()
+
+func interact():
+	match current_state:
+		State.WAITING_FOR_PLAYER:
+			emit_signal("order_placed", order_item)
+			current_state = State.ORDER_TAKEN
+			$PatienceTimer.wait_time = 50.0
+			$PatienceTimer.start()
+		State.ORDER_TAKEN:
+			print("Order already taken, waiting for food")
+
+func receive_food():
+	current_state = State.EATING
+	$PatienceTimer.stop()
+	$EatingTimer.start()
+
+func _on_patience_expired():
+	emit_signal("patience_expired")
+	queue_free()
+
+func _on_finished_eating():
+	emit_signal("finished_eating")
+	queue_free()
