c7b754f962f952b0d1e7231dd4d78a720741da74
Jo Johnson <jojohnson.jsj@gmail.com>
2026-03-04 00:59:25 -0800

    Making first frame of game use idle png

--- src/game-jam-26/scripts/Player.gd
+++ src/game-jam-26/scripts/Player.gd
@@ -15,7 +15,7 @@ func _ready():
 	$OrderConnectionTimer.wait_time = 0.5
 	$OrderConnectionTimer.one_shot = true
 	$OrderConnectionTimer.timeout.connect(_on_order_connection_timeout)
-
+	
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	
