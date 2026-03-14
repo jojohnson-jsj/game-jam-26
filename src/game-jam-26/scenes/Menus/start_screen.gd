40c997f0daf0dfad565aa51a5e7451dc28399632
Sarah-Z0 <sarahzh002@gmail.com>
2026-03-14 15:34:05 -0700

    finished home screen

--- src/game-jam-26/scenes/Menus/start_screen.gd
+++ src/game-jam-26/scenes/Menus/start_screen.gd
@@ -20,3 +20,7 @@ func _on_pull_button_pressed() -> void:
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_inventory_button_pressed() -> void:
+	pass # Replace with function body.
