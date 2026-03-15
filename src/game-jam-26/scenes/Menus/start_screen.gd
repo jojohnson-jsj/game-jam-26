3507291e952c6fec161b620420afae03dbe9646d
Sarah-Z0 <sarahzh002@gmail.com>
2026-03-14 18:04:26 -0700

    meow

--- src/game-jam-26/scenes/Menus/start_screen.gd
+++ src/game-jam-26/scenes/Menus/start_screen.gd
@@ -24,3 +24,7 @@ func _on_quit_button_pressed() -> void:
 
 func _on_inventory_button_pressed() -> void:
 	pass # Replace with function body.
+
+
+func _on_cat_button_pressed() -> void:
+	$catButton/AudioStreamPlayer.play()
