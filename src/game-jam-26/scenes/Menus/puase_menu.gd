060830d0a0a1d00f7af24cea2396c0d63a32151e
Sarah-Z0 <sarahzh002@gmail.com>
2026-03-17 20:36:02 -0700

    pause menu forgor to add

---
+++ src/game-jam-26/scenes/Menus/puase_menu.gd
@@ -0,0 +1,19 @@
+extends ColorRect
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	visible = false
+	process_mode = Node.PROCESS_MODE_ALWAYS
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _unhandled_input(event: InputEvent) -> void:
+	if(event.is_action_pressed("pause")):
+		var isPause = !get_tree().paused
+		get_tree().paused = isPause
+		visible = isPause
+		print(isPause)
