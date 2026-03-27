9a49e6c4ebb30125b909a7a22bee88a2d150aff0
Meggie Cheng <meggielcheng@gmail.com>
2026-03-27 09:39:13 -0700

    moved oven locations

---
+++ src/game-jam-26/scripts/SoundManager.gd
@@ -0,0 +1,13 @@
+extends Node
+
+func play_sfx(stream: AudioStream, volume_db := 0, pitch := 1.0):
+	var player = AudioStreamPlayer.new()
+	add_child(player)
+
+	player.stream = stream
+	player.volume_db = volume_db
+	player.pitch_scale = pitch
+	player.bus = "SFX"
+
+	player.play()
+	player.finished.connect(player.queue_free)
