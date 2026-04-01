679fabfef2842a558d9a77145fd813dae95ac5eb
Meggie Cheng <meggielcheng@gmail.com>
2026-03-31 19:46:13 -0700

    fix nihao kitty string in gacha controller script

--- src/game-jam-26/scripts/gacha_controller.gd
+++ src/game-jam-26/scripts/gacha_controller.gd
@@ -28,7 +28,7 @@ const CAT_DESCRIPTIONS = {
 	'money_cat':           ["Money Cat", "Automatically collects money so you don't have to"],
 	'host_cat':            ["Host Cat", "Click waiting groups so that the host can seat them instantly"],
 	'hopper_cat':          ["Hopper Cat", "Queue an extra order into a machine so you can load it and walk away"],
-	'nihao_cat':           ["Nihao Cat", "She makes the day last longer through her love and whimsy"],
+	'nihao_cat':           ["Nihao Cat", "This famous cat carries their franchise's popularity. It's inspiring you to carry more as well! (player can now carry 3 items)"],
 	'counter_cat':         ["Counter Cat", "Unlocks the countertop plates — drop off a finished dish and come back for it later!"],
 	'patience_cat':        ["Patience Cat", "Customers absorb his serenity, and find they have more patience than before"],
 	'cooking_cat':         ["Chef Cat", "Decreases cooking time so dishes are ready faster"],
